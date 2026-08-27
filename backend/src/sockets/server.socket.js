a839398a055a10ca709a4dee660583d08d9264c5
Abhishek kumar <abhi060807@gmail.com>
2026-08-27 23:20:06 +0530

    on 27aug:

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -6,21 +6,22 @@ let io;
 export function initSocket(httpServer){
     io = new Server(httpServer,{
         cors:{
-            origin:"http://localhost:5173",
+            origin:"https://preplexity-9cb6.onrender.com",
             credentials:true,
         }
     })
     console.log("socket io server is running ");
     
+    
+    
+    io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
 
-    io.on("connection",(socket)=>{
-        console.log("A user connected:" + socket.id);
-        
-    })
-
-    io.on("disconnect", (reason) => {
+    socket.on("disconnect", (reason) => {
         console.log("Disconnected:", socket.id, reason);
     });
+});
+
 }
 
 
