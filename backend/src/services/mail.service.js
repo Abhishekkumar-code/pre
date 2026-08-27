253b68dfe2e4db21ffc1ae1d109f8e3cdf3dccc8
Abhishek kumar <abhi060807@gmail.com>
2026-08-27 20:37:21 +0530

    Update email sender domain

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -69,7 +69,6 @@
 //   console.log("Email sent successfully");
 //   }
 
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -79,7 +78,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, html, text }) {
     const { data, error } = await resend.emails.send({
-        from: " Perplexity <onboarding@resend.dev>",
+        from: "Preplexity <no-reply@pre-edu.in>",
         to: [to],
         subject,
         html,
